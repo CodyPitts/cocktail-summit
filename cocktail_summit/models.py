d5ad66b62a4e2b2a26bd1f617d23a796c270bd5a
Cody Pitts <cody.pitts@gmail.com>
2016-09-09 13:16:27 -0700

    cleared db, made changes to models (added Event)

--- cocktail_summit/models.py
+++ cocktail_summit/models.py
@@ -4,6 +4,16 @@ from django.utils import timezone
 from django.db import models
 
 
+class Event(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField()
+    city = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return "{0}-{1}, {2}".format(self.start_date, self.end_date, self.city)
+
+
 class Speaker(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -18,6 +28,7 @@ class Sponsor(models.Model):
     logo = models.ImageField(blank=True)
     name = models.CharField(max_length=200)
     link = models.URLField()
+    event = models.ManyToManyField(Event, blank=True)
 
     def __str__(self):
         return self.name
@@ -25,11 +36,13 @@ class Sponsor(models.Model):
 
 class Session(models.Model):
     title = models.CharField(max_length=200)
-    scheduled_time = models.DateTimeField()
+    start_time = models.DateTimeField(blank=True, null=True)
+    end_time = models.DateTimeField(blank=True, null=True)
     room = models.CharField(max_length=200)
     summary = models.TextField()
     sponsor = models.ForeignKey(Sponsor)
     speakers = models.ManyToManyField(Speaker)
+    event = models.ForeignKey(Event, null=True)
 
     def __str__(self):
         return self.title
