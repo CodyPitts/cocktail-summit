d117250281074e76a0646a468562157d718eb76f
Cody Pitts <cody.pitts@gmail.com>
2016-09-15 12:53:52 -0700

    added procfile, gitignore, started heroku work

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cocktail_summit.wsgi:application --log-file -
--- cocktail_summit/models.py
+++ cocktail_summit/models.py
@@ -8,7 +8,7 @@ class Event(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     city = models.CharField(max_length=50)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return "{0}-{1}, {2}".format(self.start_date, self.end_date, self.city)
@@ -17,8 +17,9 @@ class Event(models.Model):
 class Speaker(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, blank=True)
     photo = models.ImageField(blank=True)
+    affiliation = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return "{0} {1}".format(self.first_name, self.last_name)
@@ -40,9 +41,9 @@ class Session(models.Model):
     end_time = models.DateTimeField(blank=True, null=True)
     room = models.CharField(max_length=200)
     summary = models.TextField()
-    sponsor = models.ForeignKey(Sponsor)
+    sponsor = models.ForeignKey(Sponsor, blank=True)
     speakers = models.ManyToManyField(Speaker)
-    event = models.ForeignKey(Event, null=True)
+    event = models.ForeignKey(Event)
 
     def __str__(self):
         return self.title
