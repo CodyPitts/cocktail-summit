73f0b56d5ee8179ad8fbee780d52d351cfd3ab82
Cody Pitts <cody.pitts@gmail.com>
2016-09-29 16:03:10 -0700

    made model changes, fewer mandatory fields

--- cocktail_summit/models.py
+++ cocktail_summit/models.py
@@ -8,18 +8,19 @@ from django.db import models
 
 
 class Event(models.Model):
-    start_date = models.DateField()
-    end_date = models.DateField()
-    city = models.CharField(max_length=50)
+    event_name = models.CharField(max_length=50)
+    start_date = models.DateField(blank=True)
+    end_date = models.DateField(blank=True)
+    city = models.CharField(max_length=50, blank=True)
     description = models.TextField(blank=True)
 
     def __str__(self):
-        return "{0}-{1}, {2}".format(self.start_date, self.end_date, self.city)
+        return self.event_name
 
 
 class Speaker(models.Model):
     first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30, blank=True)
     title = models.CharField(max_length=200, blank=True)
     photo = models.ImageField(blank=True)
     affiliation = models.CharField(max_length=50, blank=True)
@@ -31,7 +32,7 @@ class Speaker(models.Model):
 class Sponsor(models.Model):
     logo = models.ImageField(blank=True)
     name = models.CharField(max_length=200)
-    link = models.URLField()
+    link = models.URLField(blank=True)
     event = models.ManyToManyField(Event, blank=True)
 
     def __str__(self):
@@ -40,12 +41,12 @@ class Sponsor(models.Model):
 
 class Session(models.Model):
     title = models.CharField(max_length=200)
-    start_time = models.DateTimeField(blank=True, null=True)
-    end_time = models.DateTimeField(blank=True, null=True)
-    room = models.CharField(max_length=200)
-    summary = models.TextField()
+    start_time = models.DateTimeField(blank=True)
+    end_time = models.DateTimeField(blank=True)
+    room = models.CharField(max_length=200, blank=True)
+    summary = models.TextField(blank=True)
     sponsor = models.ForeignKey(Sponsor, blank=True)
-    speakers = models.ManyToManyField(Speaker)
+    speakers = models.ManyToManyField(Speaker, blank=True)
     event = models.ForeignKey(Event)
 
     def __str__(self):
