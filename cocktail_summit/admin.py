11ee5146f5f5bd9e0c06750914630f4ccb6909a7
Cody Pitts <cody.pitts@gmail.com>
2016-10-06 15:29:39 -0700

    forgot to register Event model in admin.py

--- cocktail_summit/admin.py
+++ cocktail_summit/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Speaker, Sponsor, Session
+from .models import Speaker, Sponsor, Session, Event
 
 admin.site.register(Speaker)
 admin.site.register(Sponsor)
 admin.site.register(Session)
+admin.site.register(Event)
