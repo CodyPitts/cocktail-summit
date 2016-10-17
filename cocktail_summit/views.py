1077d0218d6b3b12d39301c72600c2124023aa53
Cody Pitts <cody.pitts@gmail.com>
2016-10-17 16:28:51 -0700

    bookmark before adding django celery

--- cocktail_summit/views.py
+++ cocktail_summit/views.py
@@ -22,9 +22,9 @@ class PublishView(generic.base.View):
         response_message = 'Build triggered!'
 
         try:
-            management.call_command('staticsitegen', verbosity=0)
+            #management.call_command('staticsitegen', verbosity=0)
 
-            """StaticSiteRenderer.initialize_output()
+            StaticSiteRenderer.initialize_output()
 
             for Renderer in get_static_renderers():
                 r = Renderer()
@@ -35,7 +35,7 @@ class PublishView(generic.base.View):
             if MEDUSA_COLLECT_STATIC:
                 # collect static media for deployment
                 call_command('collectstatic', interactive=False,
-                    ignore_patterns=MEDUSA_COLLECT_STATIC_IGNORE)"""
+                    ignore_patterns=MEDUSA_COLLECT_STATIC_IGNORE)
 
         except:
             response_message = 'Uh oh there was a problem'
