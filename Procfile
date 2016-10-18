ef160109fddceb03a711a5765e0ba4b5c9696208
Cody Pitts <cody.pitts@gmail.com>
2016-10-18 11:12:51 -0700

    attempting to configure celery on heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn cocktail_summit.wsgi:application --log-file -
+worker: celery worker --app=tasks.app
--- cocktail_summit/settings.py
+++ cocktail_summit/settings.py
@@ -169,6 +169,15 @@ AWS_SECRET_ACCESS_KEY = env.str('AWS_SECRET_ACCESS_KEY', None)
 AWS_STORAGE_BUCKET_NAME = env.str('AWS_STORAGE_BUCKET_NAME', None)
 
 
+# For using Celery for tasks
+
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],)
+
+
+
+
+
+
 
 
 
